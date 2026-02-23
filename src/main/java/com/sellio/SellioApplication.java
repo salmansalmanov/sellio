89858398c8e2ba87666a573b0891d9a8558fe2e4
salmansalmanov <salmanov2904@gmail.com>
2026-02-23 21:49:31 +0400

    Initial commit

---
+++ src/main/java/com/sellio/SellioApplication.java
@@ -0,0 +1,13 @@
+package com.sellio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sellio/SellioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
