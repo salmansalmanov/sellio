640e0ff7c767aeb3cbfc7181933a99c19add72c9
salmansalmanov <salmanov2904@gmail.com>
2026-02-25 19:52:27 +0400

    Swagger integrated

---
+++ src/main/java/com/sellio/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.sellio.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Sellio REST API",
+                version = "1.0",
+                description = "Sellio API Documentation",
+                contact = @Contact(name = "Sellio")
+        ),
+        security = {@SecurityRequirement(name = "bearerToken")}
+)
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "bearerToken",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT"
+        )
+})
+public class SwaggerConfig {
+
+}
