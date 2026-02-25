9f6b42d2f599270e737b05007d7e4a496e0a164a
salmansalmanov <salmanov2904@gmail.com>
2026-02-25 20:03:38 +0400

    Super Admin integrated

---
+++ src/main/java/com/sellio/model/entity/SuperAdminEntity.java
@@ -0,0 +1,18 @@
+package com.sellio.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "super_admins")
+public class SuperAdminEntity extends AccountEntity {
+    private String username;
+}
