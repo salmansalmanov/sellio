5f6e56cac4478d861e5f8fec2af811794352ac90
salmansalmanov <salmanov2904@gmail.com>
2026-03-15 17:00:50 +0400

    RefreshTokenEntity implemented

---
+++ src/main/java/com/sellio/model/entity/RefreshTokenEntity.java
@@ -0,0 +1,28 @@
+package com.sellio.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "refresh_tokens")
+public class RefreshTokenEntity extends BaseEntity {
+    private UUID refreshToken;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+    private Boolean revoked;
+    private LocalDateTime expireDate;
+}
